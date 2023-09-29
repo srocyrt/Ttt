8877598a9075d040c1ade8f00463c205995cd42c
TryCo <4598163+srocyrt@users.noreply.github.com>
2023-09-30 01:05:20 +0800

    2

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,259 @@
 mod screen;
+
+use std::io::Cursor;
+
+// use screen::BitMap;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn old(w: usize, h: usize) -> String {
-    let map = screen::BitMap::white_noise(w, h);
-    format!("{}", map)
+pub struct GifDisplayer {
+    decoder: gif::Decoder<Cursor<Vec<u8>>>,
+    global_palette: Vec<u8>,
+    scale: usize,
+    width: usize,
+    height: usize,
+    screen: screen::Screen,
+    bms: Vec<screen::BitMap>,
 }
+
+pub struct GiffDisplayer {
+    decoder: gif::Decoder<std::fs::File>,
+    global_palette: Vec<u8>,
+    scale: usize,
+    width: usize,
+    height: usize,
+    screen: screen::Screen,
+}
+
+impl GiffDisplayer {
+    pub fn new(file: std::fs::File) -> Self {
+        let options = gif::DecodeOptions::new();
+        let decoder = options.read_info(file).unwrap();
+        let global_palette: Vec<u8> = decoder
+            .global_palette()
+            .unwrap()
+            .into_iter()
+            .cloned()
+            .collect();
+        let width = decoder.width() as usize;
+        let height = decoder.height() as usize;
+        let scale: usize = std::cmp::max(width / 64 + 1, height / 64 + 1);
+        let width = (width - width % scale) as usize;
+        let height = (height - height % scale) as usize;
+        // let screen = screen::Screen::with_bg(width / scale, height / scale, 0x2C2C2CFF);
+        let screen = screen::Screen::with_bg(width / scale, height / scale, 0xFFFFFFFF);
+        GiffDisplayer {
+            decoder,
+            global_palette,
+            scale,
+            width,
+            height,
+            screen,
+        }
+    }
+
+    pub fn to_string(&mut self) -> String {
+        format!("{}", self.screen)
+    }
+
+    pub fn diff(&mut self) -> *const u8 {
+        self.screen.diff()
+    }
+
+    pub fn diff_count(&self) -> u32 {
+        self.screen.diff_count()
+    }
+
+    pub fn next(&mut self) -> Result<(), ()>{
+        // let mut bitmap = screen::BitMap::new(self.width, self.height);
+        if let Some(frame) = self.decoder.read_next_frame().unwrap() {
+            print!("[{:06}, {:06}]", frame.left, frame.top);
+            print!("[{:06}, {:06}]", frame.width, frame.height);
+            print!("[{:06}, {:06}]\n", frame.buffer.len(), frame.delay);
+            // for x in (0..self.width).step_by(self.scale) {
+            //     for y in (0..self.height).step_by(self.scale) {
+            //         let mut color: (u32, u32, u32, u32) = (0, 0, 0, 0);
+            //         for dx in 0..self.scale {
+            //             for dy in 0..self.scale {
+            //                 if let Some(offset) =
+            //                     frame.buffer.get((y + dy) * frame.width as usize + (x + dx))
+            //                 {
+            //                     let offset = *offset as usize;
+            //                     if let Some(ref p) = frame.palette {
+            //                         color.0 += p[offset * 3 + 0] as u32;
+            //                         color.1 += p[offset * 3 + 1] as u32;
+            //                         color.2 += p[offset * 3 + 2] as u32;
+            //                     } else {
+            //                         color.0 += self.global_palette[offset * 3 + 0] as u32;
+            //                         color.1 += self.global_palette[offset * 3 + 1] as u32;
+            //                         color.2 += self.global_palette[offset * 3 + 2] as u32;
+            //                     }
+            //                 } else {
+            //                     println!("width: {}, height: {}, size: {}", frame.width, frame.height, frame.buffer.len());
+            //                     println!("left: {}, top: {}", frame.left, frame.top);
+            //                     println!("x: {} dx: {} y: {} dy: {}", x, dx, y, dy);
+            //                     println!("index: {}\n", (y + dy) * frame.width as usize + (x + dx));
+            //                 }
+            //             }
+            //         }
+            //         let color = match color {
+            //             // Tofix
+            //             (r, g, b, _) => [0, (b >> 2) as u8, (g >> 2) as u8, (r >> 2) as u8],
+            //         };
+            //         bitmap.set_pixel(x / self.scale, y / self.scale, u32::from_le_bytes(color));
+            //     }
+            // }
+            Ok(())
+        } else {
+            Err(())
+        }
+        // self.screen.paint(bitmap, 0, 0);
+        // self.screen.next();
+    }
+}
+
+#[wasm_bindgen]
+impl GifDisplayer {
+    pub fn new(raw_data: &[u8]) -> Self {
+        let raw_data: Vec<u8> = raw_data.to_owned();
+        let options = gif::DecodeOptions::new();
+        let decoder = options.read_info(Cursor::new(raw_data.to_owned())).unwrap();
+        let global_palette: Vec<u8> = decoder
+            .global_palette()
+            .unwrap()
+            .into_iter()
+            .cloned()
+            .collect();
+        let width = decoder.width() as usize;
+        let height = decoder.height() as usize;
+        let scale: usize = std::cmp::max(width / 64 + 1, height / 64 + 1);
+        let width = (width - width % scale) as usize;
+        let height = (height - height % scale) as usize;
+        // let screen = screen::Screen::with_bg(width / scale, height / scale, 0x2C2C2CFF);
+        let screen = screen::Screen::with_bg(width / scale, height / scale, 0xFFFFFFFF);
+        let bms = vec![];
+        GifDisplayer {
+            decoder,
+            global_palette,
+            scale,
+            width,
+            height,
+            screen,
+            bms,
+        }
+    }
+
+    pub fn to_string(&mut self) -> String {
+        format!("{}", self.screen)
+    }
+
+    pub fn diff(&mut self) -> *const u8 {
+        self.screen.diff()
+    }
+
+    pub fn diff_count(&self) -> u32 {
+        self.screen.diff_count()
+    }
+
+    pub fn next(&mut self) {
+        if let Some(frame) = self.decoder.read_next_frame().unwrap() {
+            let mut bitmap = screen::BitMap::new(self. width / self.scale, self.height / self.scale);
+            // let gif::Frame { left, top, width, height, ..} = frame;
+
+            for x in (0..self.width).step_by(self.scale) {
+                for y in (0..self.height).step_by(self.scale) {
+                    let mut color: (u32, u32, u32, u32) = (0, 0, 0, 0);
+                    for dx in 0..self.scale {
+                        for dy in 0..self.scale {
+                            if let Some(offset) =
+                                frame.buffer.get((y + dy) * frame.width as usize + (x + dx))
+                            {
+                                let offset = *offset as usize;
+                                if let Some(ref p) = frame.palette {
+                                    color.0 += p[offset * 3 + 0] as u32;
+                                    color.1 += p[offset * 3 + 1] as u32;
+                                    color.2 += p[offset * 3 + 2] as u32;
+                                } else {
+                                    color.0 += self.global_palette[offset * 3 + 0] as u32;
+                                    color.1 += self.global_palette[offset * 3 + 1] as u32;
+                                    color.2 += self.global_palette[offset * 3 + 2] as u32;
+                                }
+                            } else {
+                                panic!("no");
+                            }
+                        }
+                    }
+                    let color = match color {
+                        // Tofix
+                        (r, g, b, _) => [0, (b >> 6) as u8, (g >> 6) as u8, (r >> 6) as u8],
+                    };
+                    bitmap.set_pixel(x / self.scale, y / self.scale, u32::from_le_bytes(color));
+                }
+            }
+            self.bms.push(bitmap);
+        } else {
+            self.bms.rotate_left(1);
+        }
+        self.screen.paint(&self.bms.last().unwrap(), 0, 0);
+        // self.screen.next();
+    }
+}
+
+// #[wasm_bindgen]
+// pub fn gif(ptr: &[u8], id: usize) -> BitMap {
+//     let options = gif::DecodeOptions::new();
+//     // options.set_color_output(gif::ColorOutput::RGBA);
+//     let mut decoder = options.read_info::<&[u8]>(ptr).unwrap();
+//     let global_palette: Vec<u8> = decoder
+//         .global_palette()
+//         .unwrap()
+//         .into_iter()
+//         .cloned()
+//         .collect();
+//     let mut bm = screen::BitMap::new(128, 128);
+//     for _ in 0..id {
+//         decoder.read_next_frame();
+//     }
+//     // decoder.read_next_frame();
+//     if let Some(frame) = decoder.read_next_frame().unwrap() {
+//         let w: usize = (frame.width - (frame.width % 4)) as usize;
+//         let h: usize = (frame.height - (frame.height % 4)) as usize;
+//         for x in (0..w).step_by(4) {
+//             for y in (0..h).step_by(4) {
+//                 let mut color: (u32, u32, u32, u32) = (0, 0, 0, 0);
+//                 for dx in 0..4 {
+//                     for dy in 0..4 {
+//                         if let Some(offset) =
+//                             frame.buffer.get((y + dy) * frame.width as usize + (x + dx))
+//                         {
+//                             let offset = *offset as usize;
+//                             if let Some(ref p) = frame.palette {
+//                                 // return (p.len()).to_string();
+//                                 color.0 += p[offset * 3 + 0] as u32;
+//                                 color.1 += p[offset * 3 + 1] as u32;
+//                                 color.2 += p[offset * 3 + 2] as u32;
+//                             } else {
+//                                 color.0 += global_palette[offset * 3 + 0] as u32;
+//                                 color.1 += global_palette[offset * 3 + 1] as u32;
+//                                 color.2 += global_palette[offset * 3 + 2] as u32;
+//                                 // return format!("{:?}", color);
+//                             }
+//                         } else {
+//                             panic!("no");
+//                         }
+//                     }
+//                 }
+//                 let color = match color {
+//                     (r, g, b, a) => [0, (b >> 4) as u8, (g >> 4) as u8, (r >> 4) as u8],
+//                 };
+//                 bm.set_pixel(x / 4, y / 4, u32::from_le_bytes(color));
+//             }
+//         }
+//     }
+//     bm
+// }
+
+// #[wasm_bindgen]
+// pub fn gif(ptr: &[u8], id: usize) ->  {
+
+// }
--- src/screen/mod.rs
+++ src/screen/mod.rs
@@ -1,22 +1,35 @@
 use std::fmt;
 use wasm_bindgen::prelude::*;
 
-pub const WIDTH: usize = 256;
-pub const HEIGHT: usize = 64;
+// pub const WIDTH: usize = 256;
+// pub const HEIGHT: usize = 64;
 
-type Pixel = Option<u8>;
+type Color = u32;
 
 #[wasm_bindgen]
 pub struct BitMap {
     width: usize,
     height: usize,
-    pixels: Vec<Pixel>,
+    pixels: Vec<Color>,
+}
+
+impl std::ops::Index<usize> for BitMap {
+    type Output = Color;
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.pixels[index]
+    }
+}
+
+impl std::ops::IndexMut<usize> for BitMap {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.pixels[index]
+    }
 }
 
 #[wasm_bindgen]
 impl BitMap {
     pub fn new(width: usize, height: usize) -> Self {
-        let pixels = vec![None; width * height];
+        let pixels = vec![0; width * height];
         assert_eq!(width * height, pixels.capacity());
         BitMap {
             width,
@@ -25,15 +38,14 @@ impl BitMap {
         }
     }
 
-    pub fn fill(&mut self, color: u8) {
-        self.pixels.fill(Some(color));
+    pub fn fill(&mut self, color: Color) {
+        self.pixels.fill(color);
     }
 
     pub fn white_noise(width: usize, height: usize) -> Self {
-        let mut pixels = Vec::with_capacity(width * height);
-        for _ in 0..width * height {
-            pixels.push(Some(rand::random()));
-        }
+        let pixels = std::iter::repeat_with(|| rand::random())
+            .take(width * height)
+            .collect();
         BitMap {
             width,
             height,
@@ -41,32 +53,41 @@ impl BitMap {
         }
     }
 
-    pub fn set_pixel(&mut self, x: usize, y: usize, p: Pixel) {
-        if x > self.width { unreachable!() };
-        if y > self.height { unreachable!() };
-        self.pixels[self.width * y + x] = p;
+    pub fn set_pixel(&mut self, x: usize, y: usize, c: Color) {
+        if x > self.width {
+            unreachable!()
+        };
+        if y > self.height {
+            unreachable!()
+        };
+        self.pixels[self.width * y + x] = c;
     }
 
-    pub fn get_pixel(&self, x: usize, y: usize) -> Pixel {
-        if x > self.width { unreachable!() };
-        if y > self.height { unreachable!() };
+    pub fn get_pixel(&self, x: usize, y: usize) -> Color {
+        if x > self.width {
+            unreachable!()
+        };
+        if y > self.height {
+            unreachable!()
+        };
         self.pixels[self.width * y + x]
     }
-
 }
 
 // ▄█▀
 impl fmt::Display for BitMap {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for row in 0..self.height {
+        if self.height % 2 != 0 {
+            todo!()
+        }
+        for row in (0..self.height).step_by(2) {
             for col in 0..self.width {
+                let fore = self.get_pixel(col, row).to_le_bytes();
+                let back = self.get_pixel(col, row + 1).to_le_bytes();
                 write!(
                     f,
-                    "\u{001b}[38;5;{}m█\u{001b}[0m",
-                    match self.get_pixel(col, row) {
-                        Some(x) => x,
-                        None => 0,
-                    }
+                    "\u{001b}[38;2;{:03};{:03};{:03};48;2;{:03};{:03};{:03}m▀\u{001b}[0m",
+                    fore[3], fore[2], fore[1], back[3], back[2], back[1]
                 )?;
             }
             write!(f, "\n")?;
@@ -79,7 +100,7 @@ impl fmt::Display for BitMap {
 pub struct Screen {
     width: usize,
     height: usize,
-    background: Option<u8>,
+    background: Option<Color>,
     frame: BitMap,
     buffer: BitMap,
     diff_count: u32,
@@ -98,7 +119,7 @@ impl Screen {
         }
     }
 
-    pub fn with_bg(width: usize, height: usize, bg: u8) -> Self {
+    pub fn with_bg(width: usize, height: usize, bg: Color) -> Self {
         let mut frame = BitMap::new(width, height);
         let mut buffer = BitMap::new(width, height);
         frame.fill(bg);
@@ -113,40 +134,70 @@ impl Screen {
         }
     }
 
-    pub fn paint(&mut self, sprite: BitMap, x: usize, y: usize) {
-        let height_bound: usize = if y + sprite.height > self.height { self.height - y } else { sprite.height };
-        let width_bound: usize = if x + sprite.width > self.width { self.width - x } else { sprite.width };
+    pub fn paint(&mut self, sprite: &BitMap, x: usize, y: usize) {
+        let height_bound: usize = if y + sprite.height > self.height {
+            self.height - y
+        } else {
+            sprite.height
+        };
+        let width_bound: usize = if x + sprite.width > self.width {
+            self.width - x
+        } else {
+            sprite.width
+        };
         for offset_y in 0..height_bound {
             for offset_x in 0..width_bound {
-                self.buffer.set_pixel(x + offset_x, y + offset_y, sprite.get_pixel(offset_x, offset_y))
+                // ToFix mask
+                self.buffer.set_pixel(
+                    x + offset_x,
+                    y + offset_y,
+                    sprite.get_pixel(offset_x, offset_y),
+                )
             }
         }
     }
 
     pub fn next(&mut self) -> String {
-        self.frame.fill(if let Some(x) = self.background { x } else { 0 });
+        self.frame
+            .fill(if let Some(x) = self.background { x } else { 0 });
         std::mem::swap(&mut self.frame, &mut self.buffer);
         format!("{}", self.frame)
     }
 
-    pub fn diff(&mut self) -> *const u32 {
-        let mut d: Vec<u32> = vec![];
+    pub fn diff(&mut self) -> *const u8 {
+        let mut d: Vec<u8> = vec![];
         self.diff_count = 0;
-        for i in 0..self.width * self.height {
-            match (self.buffer.pixels[i], self.frame.pixels[i]) {
-                (Some(x), Some(y)) if x == y => {},
-                _ => {
-                    d.push(i as u32);
-                    d.push(match self.buffer.pixels[i] {
-                        Some(x) => x as u32,
-                        None => 0u32,
-                    });
-                    self.diff_count += 1;
 
+        for row in (0..self.height).step_by(2) {
+            for col in 0..self.width {
+                match (
+                    self.buffer.get_pixel(col, row),
+                    self.frame.get_pixel(col, row),
+                    self.buffer.get_pixel(col, row + 1),
+                    self.frame.get_pixel(col, row + 1),
+                ) {
+                    (b1, f1, b2, f2) if b1 == f1 && b2 == f2 => {
+                        continue;
+                    }
+                    (b1, _, b2, _) => {
+                        d.push(row as u8);
+                        d.push(col as u8);
+                        let b = b1.to_le_bytes();
+                        d.push(b[3]);
+                        d.push(b[2]);
+                        d.push(b[1]);
+                        let b = b2.to_le_bytes();
+                        d.push(b[3]);
+                        d.push(b[2]);
+                        d.push(b[1]);
+                        self.diff_count += 1;
+                    }
                 }
             }
         }
-        self.frame.fill(if let Some(x) = self.background { x } else { 0 });
+
+        self.frame
+            .fill(if let Some(x) = self.background { x } else { 0 });
         std::mem::swap(&mut self.frame, &mut self.buffer);
         d.as_ptr()
     }
